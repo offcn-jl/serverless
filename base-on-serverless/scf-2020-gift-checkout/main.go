5ae0249bf882e9013582dc4fec423aabb4e5138b
冯国清 <master@rebeta.cn>
2020-05-15 14:02:41 +0800

    feat : decoupling handler;

--- base-on-serverless/scf-2020-gift-checkout/main.go
+++ base-on-serverless/scf-2020-gift-checkout/main.go
@@ -10,39 +10,29 @@
 package main
 
 import (
-	"github.com/offcn-jl/cscf"
-	"github.com/offcn-jl/cscf/fake-http"
 	"github.com/offcn-jl/go-common/database/orm"
-	"serverless/common/database/orm/structs"
+	"github.com/offcn-jl/gscf"
+	"serverless/common/handler/2020/gift"
 	"serverless/common/middleware"
 )
 
 // 接口版本号
-var version = "0.1.0"
+var version = "0.2.0"
 
 // 接口构建时间, 将会在编译时注入
 var builtTime = ""
 
 func main() {
 	// 使用默认引擎
-	r := chaos.Default()
+	r := gin.Default()
 
 	// 添加中间件及处理函数
 	// 处理函数要作为最后一个参数传入
-	r.Use(middleware.AddVersions(version+builtTime), middleware.CheckOrigin(), MainHandler)
+	r.Use(middleware.AddVersions(version+builtTime), middleware.CheckOrigin(), gift.GetCheckout)
 
 	// 启动框架, 开始监听请求
 	r.Run()
 
 	// 在程序结束时关闭 ORM 的连接
 	defer orm.Close()
-}
-
-// MainHandler 处理函数
-// 本接口用于查询获奖信息
-func MainHandler(c *chaos.Context) {
-	giftCheckInfo := structs.EventsGift{}
-	orm.PostgreSQL.Where("Name = ? AND Phone = ?", c.Param("Name"), c.Param("Phone")).First(&giftCheckInfo)
-
-	c.JSON(http.StatusOK, giftCheckInfo)
 }
--- base-on-serverless/scf-2020-gift-surplus/main.go
+++ base-on-serverless/scf-2020-gift-surplus/main.go
@@ -10,43 +10,29 @@
 package main
 
 import (
-	"github.com/offcn-jl/cscf"
-	"github.com/offcn-jl/cscf/fake-http"
 	"github.com/offcn-jl/go-common/database/orm"
-	"serverless/common/database/orm/structs"
+	"github.com/offcn-jl/gscf"
+	"serverless/common/handler/2020/gift"
 	"serverless/common/middleware"
 )
 
 // 接口版本号
-var version = "0.1.0"
+var version = "0.2.0"
 
 // 接口构建时间, 将会在编译时注入
 var builtTime = ""
 
 func main() {
 	// 使用默认引擎
-	r := chaos.Default()
+	r := gin.Default()
 
 	// 添加中间件及处理函数
 	// 处理函数要作为最后一个参数传入
-	r.Use(middleware.AddVersions(version+builtTime), middleware.CheckOrigin(), MainHandler)
+	r.Use(middleware.AddVersions(version+builtTime), middleware.CheckOrigin(), gift.GetSurplus)
 
 	// 启动框架, 开始监听请求
 	r.Run()
 
 	// 在程序结束时关闭 ORM 的连接
 	defer orm.Close()
-}
-
-// MainHandler 处理函数
-// 本接口用来获取礼品的剩余数量
-func MainHandler(c *chaos.Context) {
-	// 定义计数变量
-	count := 0
-
-	// 取出数量
-	orm.PostgreSQL.Model(structs.EventsGift{}).Where("Name = ? AND Phone IS NULL", c.Param("Name")).Count(&count)
-
-	// 返回数据
-	c.JSON(http.StatusOK, chaos.H{"Surplus": count})
 }
