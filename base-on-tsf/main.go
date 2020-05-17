8cbf004358680aac1a2943d09eba930f3aa12e72
冯国清 <master@rebeta.cn>
2020-05-17 16:31:46 +0800

    feat : add apis;

--- base-on-tsf/main.go
+++ base-on-tsf/main.go
@@ -12,8 +12,10 @@ import (
 	"github.com/offcn-jl/go-common/database/orm"
 	"github.com/offcn-jl/go-common/logger"
 	"tsf/common/database/orm/structs"
-	"tsf/common/handler"
 	"tsf/common/handler/2020/05/01"
+	"tsf/common/handler/2020/05/02"
+	"tsf/common/handler/app"
+	"tsf/common/handler/photo"
 	"tsf/common/middleware"
 )
 
@@ -29,22 +31,51 @@ func main() {
 	r.Use(middleware.AddVersions())
 
 	// 照片处理路由
-	r.POST("/photo/:Beauty", handler.Photo)
+	r.POST("/photo/:Beauty", photo.PostHandler)
+
+	// 应用程序路由组
+	appGroup := r.Group("/app")
+	{
+		// 版本控制路由组
+		versionControlGroup := appGroup.Group("/version-control")
+		{
+			// GetVersion 获取应用程序版本控制信息
+			versionControlGroup.GET("/get/:AppID", app.GetVersion)
+		}
+	}
 
 	// 2020 年路由组 ( Group Year 2020 )
 	GY2020 := r.Group("/2020", middleware.CheckOrigin())
 	{
-		// 4 月路由组 ( Group Month 05 )
-		GM04 := GY2020.Group("/05")
+		// 5 月路由组 ( Group Month 05 )
+		GM05 := GY2020.Group("/05")
 		{
 			// 活动 01 路由组 ( Group Event 01 )
-			GE01 := GM04.Group("/01")
+			// 可用于带计数的预约类活动
+			GE01 := GM05.Group("/01")
 			{
 				// PostSubscribe 处理用户订阅 ( 参与活动 ) 事件
 				GE01.POST("/:Name/subscribe/:Phone", h20200501.PostSubscribe)
 
 				// GetCount 获取订阅活动的用户数量
 				GE01.GET("/:Name/count", h20200501.GetCount)
+			}
+
+			// 活动 02 路由组
+			// 礼品 ( 兑换码 ) 发放类
+			GE02 := GM05.Group("/02")
+			{
+				// GetSurplus 获取礼品剩余数量
+				GE02.GET("/:Name/surplus", h20200502.GetSurplus)
+
+				// GetGift 获取礼品
+				GE02.GET("/:Name/get/:Phone", h20200502.GetGift)
+
+				// GetCheckout 查询领取信息
+				GE02.GET("/:Name/checkout/:Phone", h20200502.GetCheckout)
+
+				// PatchConsume 消费礼品
+				GE02.PATCH("/:Name/consume/:Phone/detail/:ConsumeDetail", h20200502.PatchConsume)
 			}
 		}
 	}
